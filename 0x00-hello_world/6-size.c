35baed26620dcf33b89fe8ea6df9e145a700940a
Biggity231 <udensikingsleyezemeka@gmail.com>
2023-03-16 17:02:47 -0700

    goat

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - Entry point
-* 
-* Return: Always 0 (Success)
-*/
+ * main - A program that prints the size of various computer types
+ * Return 0 (Success)
+ */
 int main(void)
 {
-         printf("with proper grammar, but the outcome is a piece of art, \n");
-         return (O);
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
 
