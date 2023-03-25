8af5e399f498f340c754a55f7444a57170af6ff2
Biggity231 <udensikingsleyezemeka@gmail.com>
2023-03-24 18:03:46 -0700

    /0-isuper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
-#define MAIN H
+#define MAIN_H
 
-int _putchar(char c);yu
-int_isupper(int c);
+int _putchar(char c);
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
@@ -10,7 +10,8 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-void print_square (int size);
+void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
 
 #endif
